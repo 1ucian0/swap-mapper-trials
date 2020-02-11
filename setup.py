56cbe54f10966e3b886b9a98b46c0baf339d2400
Luciano Bello <luciano.bello@ibm.com>
2020-02-11 10:58:20 -0500

    code

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="swap-mapper-trials",
+    description="More trials are better results(?)",
+    author="gines-carrascal",
+    author_email="gines-carrascal@qiskit.org",
+    packages=find_packages(exclude=['test*']),
+    install_requires=["qiskit"]
+)
